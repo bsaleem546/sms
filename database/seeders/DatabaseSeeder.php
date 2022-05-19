f34c396df2aa94393fc67e90130afac8b914acc0
unknown <hsntkhan1614@gmail.com>
2022-05-19 16:44:38 +0500

    salary worth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class   DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+//        $this->call(PermissionTableSeeder::class);
+//        $this->call(CreateAdminUserSeeder::class);
+        $this->call(staffAttSeeder::class);
         // \App\Models\User::factory(10)->create();
+
     }
 }
