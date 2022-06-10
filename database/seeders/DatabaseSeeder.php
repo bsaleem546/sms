73fc84e5908946df839317f9b5c6f0a6771b17e4
bsaleem546 <m.bilal2145@gmail.com>
2022-06-10 16:04:43 +0500

    permoission on

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,10 @@ class   DatabaseSeeder extends Seeder
     {
         $this->call(PermissionTableSeeder::class);
         $this->call(CreateAdminUserSeeder::class);
-//        $this->call(staffAttSeeder::class);
+        $this->call(staffAttSeeder::class);
 //         \App\Models\User::factory(10)->create();
-//        $this->call(ClassSeeder::class);
-//        $this->call(StudentSeeder::class);
-//        $this->call(SubjectSeeder::class);
+        $this->call(ClassSeeder::class);
+        $this->call(StudentSeeder::class);
+        $this->call(SubjectSeeder::class);
     }
 }
